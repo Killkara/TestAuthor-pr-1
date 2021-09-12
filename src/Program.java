d95a31bb65bea2da84bacf2bad80b96dda52fbfd
Killkara <killkara21@gmail.com>
2021-09-12 20:39:43 +0300

    Initial commit

---
+++ src/Program.java
@@ -0,0 +1,7 @@
+public class Program {
+    public static void main(String[] args) {
+        Author a= new Author("Кирилл","mailbox@mail.ru",'m');
+        System.out.printf(a.toString());
+    }
+}
+
